36f0a4a13b1305c88e56a44795fe7d465f90160d
Constantine Ratkin <r-const@yandex.ru>
2022-11-19 16:24:53 +0300

    code addition

---
+++ StringMassiveTask/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string [] baseArray = new string [7] {"2", "monday", "try", "it", "three", "2542", "now"};
+
+string [] emptyArray = new string[] { };
+
+string [] selection (string [] baseArray, string [] emptyArray, int a = 3)
+{
+
+
+    for (int i = 0; i< baseArray.Length; i++)
+    {
+        for (int j = 0; baseArray[i].Length<=a; j++)
+        {
+            emptyArray[j] = baseArray[i];
+        }
+       
+    }
+return emptyArray;
+        
+}
+
+ Console.Write(selection(baseArray, emptyArray, 3));
